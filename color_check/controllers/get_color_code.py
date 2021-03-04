246c58a6e9a63ff03a229a248e90f6f795d03b42
Alex Costa <alexandre.costa@code.berlin>
2021-03-04 18:34:23 +0100

    commented out logging

--- color_check/controllers/get_color_code.py
+++ color_check/controllers/get_color_code.py
@@ -6,11 +6,11 @@
 # for example by logging the request and error, and the user,
 # letting them know that their color doesn't exist.
 import json
-import logging
+# import logging
 
-logging.basicConfig(filename='/tmp/log.txt',
-                    level=logging.INFO,
-                    format='%(asctime)s - %(levelname)s - "%(message)s"')
+# logging.basicConfig(filename='/tmp/log.txt',
+#                     level=logging.INFO,
+#                     format='%(asctime)s - %(levelname)s - "%(message)s"')
 
 
 def get_color_code(color_name):
@@ -19,13 +19,13 @@ def get_color_code(color_name):
     # The file can be considered as JSON format, or as a Python dictionary.
 
     # log user color
-    logging.info(color_name)
+    # logging.info(color_name)
 
     # controlling for upper case and leading/trailing spaces
     try:
         color_name = color_name.lower().strip()
     except AttributeError:
-        logging.error("User did not submit a string")
+        # logging.error("User did not submit a string")
         pass
 
     try:
@@ -33,7 +33,7 @@ def get_color_code(color_name):
             data = json.load(f)
             color_hex_code = data[color_name]
     except KeyError:
-        logging.error("'%s' is not recognized", color_name)
+        # logging.error("'%s' is not recognized", color_name)
         raise TypeError("Submitted string does't have a match in JSON file")
 
     return color_hex_code
